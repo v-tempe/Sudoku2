db338df3c6773a76226cd26913934932f7a6ab11
Stepan Kutnenko <stepankutnenko@yandex.ru>
2025-04-19 22:47:32 +0300

    add filling the sudoku (in the simplest way)

--- black_cell.py
+++ black_cell.py
@@ -6,5 +6,9 @@ class BlackCell(Cell):
     def __init__(self, true_value: Optional[int]):
         Cell.__init__(self, true_value)
 
+    @Cell.true_value.setter
+    def true_value(self, new_value):
+        self._true_value = new_value
+
     def is_filled(self) -> bool:
         return self._true_value is not None
--- sudoku.py
+++ sudoku.py
@@ -4,7 +4,14 @@ from black_cell import BlackCell
 
 class Sudoku:
     def __init__(self):
+        def fill_grid():
+            for j in range(9):
+                for i in range(9):
+                    cell_value = (i + (j // 3) + j * 3) % 9 + 1
+                    self.grid[(j, i)].true_value = cell_value
+
         self.grid: dict[tuple[int, int]: Cell] = {(i // 9, i % 9): BlackCell(None) for i in range(81)}
+        fill_grid()
 
     def print(self):
         for j in range(9):
