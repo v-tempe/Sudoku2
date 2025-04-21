c700de34bb75728922df538d12cde625e3b4b93d
Stepan Kutnenko <stepankutnenko@yandex.ru>
2025-04-21 14:43:55 +0300

    make field "grid" and method "_replace_black_cell_with_blue_cell" in class "Sudoku" from 'public' to 'protected'

--- sudoku.py
+++ sudoku.py
@@ -9,7 +9,7 @@ class Sudoku:
             for j in range(9):
                 for i in range(9):
                     cell_value = (i + (j // 3) + j * 3) % 9 + 1
-                    self.grid[(j, i)].true_value = cell_value
+                    self._grid[(j, i)].true_value = cell_value
 
         def weed_grid():
             from random import randint
@@ -20,22 +20,22 @@ class Sudoku:
             for _ in range(amount_discarded_cells):
                 coors_next: tuple[int, int] = randint(0, 8), randint(0, 8)
                 if coors_next not in coors_discarded_cells:
-                    self.replace_black_cell_with_blue_cell(coors_next)
+                    self._replace_black_cell_with_blue_cell(coors_next)
                     coors_discarded_cells.add(coors_next)
 
-        self.grid: dict[tuple[int, int]: Cell] = {(i // 9, i % 9): BlackCell(None) for i in range(81)}
+        self._grid: dict[tuple[int, int]: Cell] = {(i // 9, i % 9): BlackCell(None) for i in range(81)}
 
         fill_grid()
         weed_grid()
 
-    def replace_black_cell_with_blue_cell(self, coors: tuple[int, int]):
-        old_cell = self.grid[coors]
+    def _replace_black_cell_with_blue_cell(self, coors: tuple[int, int]):
+        old_cell = self._grid[coors]
         new_cell = BlueCell(old_cell.true_value)
-        self.grid[coors] = new_cell
+        self._grid[coors] = new_cell
 
     def print(self):
         for j in range(9):
             for i in range(9):
-                next_cell = self.grid[(j, i)]
+                next_cell = self._grid[(j, i)]
                 print(next_cell.announced_value if next_cell.is_filled() else '_', '', end='')
             print()
