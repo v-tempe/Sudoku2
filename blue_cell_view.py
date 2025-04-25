7a1cdfab04f632a846aee03a8dd38d41794e2a4e
Stepan Kutnenko <stepankutnenko@yandex.ru>
2025-04-25 13:39:02 +0300

    add rendering of BlueCell activation/deactivation

--- blue_cell_view.py
+++ blue_cell_view.py
@@ -10,19 +10,20 @@ class BlueCellView(pygame.sprite.Sprite):
     size = 100
     border_padding = 1
 
-    STANDARD_COLOUR = colors.LIGHT_GREY
+    STANDARD_COLOR = colors.LIGHT_GREY
+    ACTIVE_COLOR = colors.AS_IF_WHITE
 
     def __init__(self, i: int, j: int):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.surface.Surface((BlueCellView.size - BlueCellView.border_padding,
                                              BlueCellView.size - BlueCellView.border_padding))
-        self.image.fill(BlueCellView.STANDARD_COLOUR)
+        self.image.fill(BlueCellView.STANDARD_COLOR)
         self.rect = self.image.get_rect()
         self.rect.center = (i * BlueCellView.size + BlueCellView.size // 2,
                             j * BlueCellView.size + BlueCellView.size // 2)
 
-    def draw(self, surf: pygame.surface.Surface, value: Optional[int]):
-        self.image.fill(BlueCellView.STANDARD_COLOUR)
+    def draw(self, surf: pygame.surface.Surface, value: Optional[int], is_active: bool):
+        self.image.fill(BlueCellView.ACTIVE_COLOR if is_active else BlueCellView.STANDARD_COLOR)
         if value is not None:
             draw_text(self.image, str(value), int(self.size * 0.8), self.size // 2, self.size // 2,
                       colors.BLUE)
