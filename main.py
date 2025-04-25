e98caca8753dd1fffd5b7986d8c9b168d5615600
Stepan Kutnenko <stepankutnenko@yandex.ru>
2025-04-25 12:50:56 +0300

    make input from graphical UI

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Optional
 
+import colors
 import constants.actions
 from blue_cell import CellManager
 
 
 def main():
-    import user_interface
+    import pygame
+
     from sudoku import Sudoku
 
     def process_actions():
@@ -14,6 +16,7 @@ def main():
         def process_choose():
             nonlocal chosen_cell
             chosen_cell = sudoku.get_cell_manager(int(action['x']), int(action['y']))
+            print(f"{chosen_cell=}")
 
         def process_set_value():
             nonlocal chosen_cell
@@ -39,16 +42,19 @@ def main():
                 process_set_value()
             elif command == constants.actions.commands.EXIT:
                 process_exit()
-            sudoku.print()
 
     def process_game_finishing():
         print(f"Sudoku is solved! Congratulations!")
+
+    pygame.init()
+    display = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+    pygame.display.set_caption(constants.GAME_NAME)
+    clock = pygame.time.Clock()
 
     sudoku = Sudoku()
 
     chosen_cell: Optional[CellManager] = None
 
-    sudoku.print()
     running = True
     while running:
         is_sudoku_solved = sudoku.is_solved()
@@ -58,11 +64,40 @@ def main():
             continue
 
         actions: list[dict[str: str]] = list()
-        raw_user_response = user_interface.text.get_raw_text_from_stdin(constants.actions.INPUT_PROMPT)
-        next_action: dict[str: str] = user_interface.text.process_text_user_response(raw_user_response)
-        actions.append(next_action)
+
+        for event in pygame.event.get():
+
+            incorrect_action: dict[str: str] = {constants.actions.keywords.INCORRECT_ACTION: ""}
+            next_action: dict[str: str] = incorrect_action.copy()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click_coors: tuple[int, int] = event.pos
+
+                coor_x, coor_y = sudoku.to_cell_dimensionality(click_coors)
+                next_action = {constants.actions.keywords.ACTION: constants.actions.commands.CHOOSE,
+                               constants.actions.keywords.X: coor_x,
+                               constants.actions.keywords.Y: coor_y}
+            elif event.type == pygame.KEYDOWN:
+                key_char = event.unicode
+                print(f"{key_char=}")
+
+                if int(key_char) in range(1, 10):
+                    next_action = {constants.actions.keywords.ACTION: constants.actions.commands.SET_VALUE,
+                                   constants.actions.keywords.VALUE: key_char}
+            elif event.type == pygame.QUIT:
+                next_action = {constants.actions.keywords.ACTION: constants.actions.commands.EXIT}
+
+            if next_action != incorrect_action:
+                print(f"{next_action=}")
+            actions.append(next_action)
 
         process_actions()
+
+        display.fill(colors.BLACK)
+        sudoku.draw(display)
+
+        pygame.display.flip()
+
+        clock.tick(constants.FPS)
 
 
 if __name__ == "__main__":
